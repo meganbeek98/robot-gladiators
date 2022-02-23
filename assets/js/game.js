a55ee51b5b3445ec28433c8ad7c58823cd41bb3c
meganbeek98 <meganbeek98@gmail.com>
2022-02-23 13:17:04 -0500

    playerName

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,9 @@
-window.alert("This is an alert! Javascript is running!");
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName); // What is this?
+console.log("This logs a string, good for leaving yourself a message");
+console.log(10 +10); // This will do math and log 20
+console.log("Our Robot's name is " + playerName);
+function fight () {
+    window.alert("The fight has begun!")
+}
+// fight();
